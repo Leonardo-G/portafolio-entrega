2d5694ab8aa4f438550d6e08789afbcd9f5d749a
leonardog9823 <guanuco43@gmail.com>
2023-01-20 21:44:04 -0300

    certificates info

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const plusButton = document.querySelectorAll(".course__plus");
+const info = document.querySelectorAll(".line");
+const course = document.querySelectorAll(".infoCourse");
+
+const handleClickPlusButton = (e) => {
+    console.log(e)
+    const indexButton = e.target.dataset.button;
+
+    const lineCourse = info[indexButton];
+    const infoCourse = course[indexButton];
+
+    e.target.classList.toggle("rotate");
+    lineCourse.classList.toggle("line--activated");
+    infoCourse.classList.toggle("infoCourse--activated");
+}
+
+plusButton.forEach( p => {
+    p.addEventListener( "click", handleClickPlusButton );
+} )
