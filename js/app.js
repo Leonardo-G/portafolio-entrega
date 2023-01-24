be17a6bb82110d3cd6913b1ca1fc232626a9b02a
leonardog9823 <guanuco43@gmail.com>
2023-01-23 22:24:31 -0300

    script links nav

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const info = document.querySelectorAll(".line");
 const course = document.querySelectorAll(".infoCourse");
 const button = document.querySelector(".nav__button");
 const navResponsive = document.querySelector("#nav-responsive");
+const body = document.querySelector("body");
+const linksResponsive = document.querySelectorAll(".responsive--links a");
 
 const handleClickPlusButton = (e) => {
     const indexButton = e.target.dataset.button;
@@ -17,9 +19,19 @@ const handleClickPlusButton = (e) => {
 
 const handleNavResponsive = () => {
     navResponsive.classList.toggle("responsive--actived");
+    body.classList.toggle("body");
+}
+
+const handleHiddenNav = () => {
+    navResponsive.classList.toggle("responsive--actived");
+    body.classList.toggle("body");
 }
 
 plusButton.forEach( p => {
     p.addEventListener( "click", handleClickPlusButton );
 } )
+
+linksResponsive.forEach( l => {
+    l.addEventListener( "click", handleHiddenNav );
+})
 button.addEventListener( "click", handleNavResponsive );
